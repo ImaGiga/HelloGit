f6b7f3d31281e2b6949d1178d89112c07d7afba8
imagiga <akio.imagawa@gmail.comakio.imagawa@gmail.com>
2019-04-29 15:50:04 +0900

    label追加

--- HelloGit/ViewController.swift
+++ HelloGit/ViewController.swift
@@ -1,10 +1,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet weak var centerLabel: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		print("Hello Git")
+		centerLabel.text = "12345"
 	}
 }
 
