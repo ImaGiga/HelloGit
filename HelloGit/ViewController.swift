fd9d6961b3d9e96411af1b379d78f83491ba1eb1
imagiga <akio.imagawa@gmail.comakio.imagawa@gmail.com>
2019-04-29 16:50:55 +0900

    print追加

--- HelloGit/ViewController.swift
+++ HelloGit/ViewController.swift
@@ -5,11 +5,11 @@ class ViewController: UIViewController {
 
 	@IBAction func tapButton(_ sender: Any) {
 		centerLabel.text = "tapped"
+		print("tapped")
 	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		print("Hello Git")
 		centerLabel.text = "12345"
 		//TODO: - make button
 	}
