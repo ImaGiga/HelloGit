ba238374dc738f3a0123266ef0c7bcdba8061608
imagiga <akio.imagawa@gmail.comakio.imagawa@gmail.com>
2019-04-29 15:44:27 +0900

    コメントを削除した printをつけた

--- HelloGit/ViewController.swift
+++ HelloGit/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  HelloGit
-//
-//  Created by IMAGAWA AKIO on 2019/04/29.
-//  Copyright © 2019 IMAGAWA AKIO. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		print("Hello Git")
 	}
-
-
 }
 
