42264d2a675c622e44c03f8e6181ac8eaac60115
imagiga <akio.imagawa@gmail.comakio.imagawa@gmail.com>
2019-04-29 16:43:55 +0900

    button tap event

--- HelloGit/ViewController.swift
+++ HelloGit/ViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 class ViewController: UIViewController {
 	@IBOutlet weak var centerLabel: UILabel!
 
+	@IBAction func tapButton(_ sender: Any) {
+		centerLabel.text = "tapped"
+	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		print("Hello Git")
