5b24948b14fdee389a8518804f4e73b2ee471569
imagiga <akio.imagawa@gmail.comakio.imagawa@gmail.com>
2019-05-04 13:12:46 +0900

    履歴をさかのぼるコメントを追加した GIT 履歴

--- HelloGit/ViewController.swift
+++ HelloGit/ViewController.swift
@@ -1,12 +1,16 @@
 import UIKit
 
+/*
+ 履歴をさかのぼる:2019年5月4日
+*/
+
 class ViewController: UIViewController {
 	@IBOutlet weak var centerLabel: UILabel!
 
 	@IBAction func tapButton(_ sender: Any) {
 		centerLabel.text = "tapped"
 	}
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		centerLabel.text = "12345"
